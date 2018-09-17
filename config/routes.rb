22f64add75f02e3a1ec2f052043b227ed1a9f987
Your Name <unknownfreq@protonmail.com>
2018-09-17 13:18:44 +0200

    added skills model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
