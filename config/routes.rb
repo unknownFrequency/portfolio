c5de10b50e0e0eb6477943f6dc8e3a8ec94d26be
Your Name <unknownfreq@protonmail.com>
2018-09-17 16:40:59 +0200

    sdzfxgchvbjmn,

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :posts
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
 
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
 end
