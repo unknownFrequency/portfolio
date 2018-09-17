f2ec5de794c846730f762678f2518ac71eff081c
Your Name <unknownfreq@protonmail.com>
2018-09-17 17:52:12 +0200

    Added toggle_status btn with action and routing etc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    member do
+      get :toggle_status
+    end
+  end
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
